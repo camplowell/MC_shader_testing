0fb6587cfc47c1b28f2216a4d0c3ce5f6f0dd135
Lolwel21 Camp <lolwel21camp@Lolwel21s-MacBook-Pro-5.local>
2022-03-03 19:21:41 -0500

    Initial commit

---
+++ shaders/lib/common.glsl
@@ -0,0 +1,47 @@
+#if !defined COMMON
+#define COMMON
+
+// ===============================================================================================
+// Camera and transformation
+// ===============================================================================================
+
+uniform mat4 gbufferModelView;
+uniform mat4 gbufferModelViewInverse;
+
+uniform mat4 gbufferProjection;
+uniform mat4 gbufferProjectionInverse;
+
+uniform mat4 gbufferPreviousModelView;
+uniform mat4 gbufferPreviousProjection;
+
+uniform mat4 shadowModelView;
+uniform mat4 shadowModelViewInverse;
+
+uniform mat4 shadowProjection;
+uniform mat4 shadowProjectionInverse;
+
+uniform vec3 cameraPosition;
+uniform float near;
+uniform float far;
+
+// ===============================================================================================
+// Settings
+// ===============================================================================================
+
+
+
+// ===============================================================================================
+// Other uniforms
+// ===============================================================================================
+
+uniform float viewWidth;
+uniform float viewHeight;
+uniform int frameCounter;
+
+// ===============================================================================================
+// Universal utilities
+// ===============================================================================================
+
+#include "/lib/space.glsl"
+
+#endif
---
+++ shaders/lib/space.glsl
@@ -0,0 +1,103 @@
+#if !defined LIB_SPACE
+#define LIB_SPACE
+
+// ===============================================================================================
+// Vertex-space definitions
+// ===============================================================================================
+
+#if defined VERTEX
+
+vec3 model2view() {
+  return (gl_ModelViewMatrix * gl_Vertex).xyz;
+}
+
+vec4 model2clip() {
+  return gl_ModelViewProjectionMatrix * gl_Vertex;
+}
+
+vec2 getTexCoord() {
+  return (gl_TextureMatrix[0] * gl_MultiTexCoord0).xy;
+}
+
+vec2 getLmCoord() {
+  return (gl_TextureMatrix[1] * gl_MultiTexCoord1).xy;
+}
+
+vec3 getNormal() {
+  return normalize(gl_NormalMatrix * gl_Normal);
+}
+
+#endif
+
+// ===============================================================================================
+// Base definitions
+// ===============================================================================================
+
+vec3 screen2clip(vec3 screenPos) {
+  return screenPos * 2.0 - 1.0;
+}
+vec3 clip2screen(vec3 clipPos) {
+  return clipPos * 0.5 + 0.5;
+}
+
+vec3 clip2view(vec3 clipPos, mat4 projectionInverse) {
+  vec4 tmp = projectionInverse * vec4(clipPos, 1.0);
+  return tmp.xyz / tmp.w;
+}
+vec3 view2clip(vec3 viewPos, mat4 projection) {
+  vec4 clip = projection * vec4(viewPos, 1.0);
+  return clip.xyz / clip.w;
+}
+
+vec3 view2eye(vec3 viewPos, mat4 modelViewInverse) {
+  return mat3(modelViewInverse) * viewPos;
+}
+vec3 eye2view(vec3 eyePos, mat4 modelView) {
+  return mat3(modelView) * eyePos;
+}
+
+vec3 eye2feet(vec3 eyePos, mat4 modelViewInverse) {
+  return eyePos + modelViewInverse[3].xyz;
+}
+vec3 feet2eye(vec3 feetPos, mat4 modelView) {
+  return feetPos + modelView[3].xyz;
+}
+
+vec3 view2feet(vec3 viewPos, mat4 modelViewInverse) {
+  return (modelViewInverse * vec4(viewPos, 1.0)).xyz;
+}
+vec3 feet2view(vec3 feetPos, mat4 modelView) {
+  return (modelView * vec4(feetPos, 1.0)).xyz;
+}
+
+vec3 feet2world(vec3 feetPos, vec3 playerPos) {
+  return feetPos + playerPos;
+}
+
+// ===============================================================================================
+// Specific definitions
+// ===============================================================================================
+
+// Player-space shorthand
+
+#define clip2view_p(_clipPos) clip2view(_clipPos, gbufferProjectionInverse)
+#define view2clip_p(_viewPos) view2clip(_viewPos, gbufferProjection)
+
+#define view2eye_p(_viewPos) view2eye(_viewPos, gbufferModelViewInverse)
+#define eye2view_p(_eyePos) eye2view(_eyePos, gbufferModelView)
+
+#define eye2feet_p(_eyePos) eye2feet(_eyePos, gbufferModelViewInverse)
+#define feet2eye_p(_feetPos) feet2eye(_feetPos, gbufferModelView)
+
+#define view2feet_p(_viewPos) view2feet(_viewPos, gbufferModelViewInverse)
+#define feet2view_p(_feetPos) feet2view(_feetPos, gbufferModelView)
+
+// Shadow-space shorthand
+
+#define view2eye_s(_viewPos) view2eye(_viewPos, shadowModelViewInverse)
+#define eye2view_s(_eyePos) eye2view(_eyePos, shadowModelView)
+
+#define view2feet_s(_viewPos) view2feet(_viewPos, shadowModelViewInverse)
+#define feet2view_s(_feetPos) feet2view(_feetPos, shadowModelView)
+
+#endif // End of document
